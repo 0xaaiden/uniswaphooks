dc6a29ccb9bf804fb929a4aee977d323fd36dc4a
FindMalek <malekgarahellalbus@gmail.com>
2023-11-24 13:37:34 +0100

    :hammer:Verified Hooks on random order on each load.

--- src/components/PopularHooks.jsx
+++ src/components/PopularHooks.jsx
@@ -4,136 +4,163 @@ import { Badge } from '@component/reusable/Badge'
 
 const uniswaplabs = [
   {
-    id: 15,
-    title: 'Full Range',
-    description:
-      'A hook that allows a Uniswap pool to provide liquidity for a range of prices. This can be used to create a market maker for a volatile asset or to provide more liquidity for a thin market.\n' +
-      '',
-    creator: 'Uniswap',
-    website: 'https://github.com/Uniswap/v4-periphery',
-    github:
-      'https://github.com/Uniswap/v4-periphery/blob/main/contracts/hooks/examples/FullRange.sol',
-    status: 'published',
-    createdAt: '2023-11-16T17:31:57.329Z',
-    updatedAt: '2023-11-16T17:37:25.048Z',
-    categoryId: 'uniswap-labs',
-    category: {
-      id: 'uniswap-labs',
-      title: 'Uniswap Labs',
-      category: 'hooks',
-      emoji: '🦄',
-      count: 0,
-      tag: 'Verified',
-      createdAt: '2023-11-16T17:36:33.658Z',
-      updatedAt: '2023-11-16T17:36:33.658Z',
-    },
-  },
-  {
-    id: 16,
-    title: 'Geomean Oracle',
-    description:
-      'A unique hook making a Uniswap pool function as an oracle. The geomean price is calculated using the prices of the assets in the pool. This can be used to get a more accurate price for an asset than a single oracle.\n' +
-      '',
-    creator: 'Uniswap',
-    website: 'https://github.com/Uniswap/v4-periphery',
-    github:
-      'https://github.com/Uniswap/v4-periphery/blob/main/contracts/hooks/examples/GeomeanOracle.sol',
-    status: 'published',
-    createdAt: '2023-11-16T17:32:33.462Z',
-    updatedAt: '2023-11-16T17:37:59.195Z',
-    categoryId: 'uniswap-labs',
-    category: {
-      id: 'uniswap-labs',
-      title: 'Uniswap Labs',
-      category: 'hooks',
-      emoji: '🦄',
-      count: 0,
-      tag: 'Verified',
-      createdAt: '2023-11-16T17:36:33.658Z',
-      updatedAt: '2023-11-16T17:36:33.658Z',
-    },
-  },
-  {
-    id: 17,
-    title: 'Limit Order',
-    description:
-      'A hook that allows users to place limit orders. This means that they can specify the price at which they are willing to buy or sell an asset. If the market price reaches the limit price, the order will be executed.\n' +
-      '',
-    creator: 'Uniswap',
-    website: 'https://github.com/Uniswap/v4-periphery',
-    github:
-      'https://github.com/Uniswap/v4-periphery/blob/main/contracts/hooks/examples/LimitOrder.sol',
-    status: 'published',
-    createdAt: '2023-11-16T17:33:02.369Z',
-    updatedAt: '2023-11-16T17:38:16.566Z',
-    categoryId: 'uniswap-labs',
-    category: {
-      id: 'uniswap-labs',
-      title: 'Uniswap Labs',
-      category: 'hooks',
-      emoji: '🦄',
-      count: 0,
-      tag: 'Verified',
-      createdAt: '2023-11-16T17:36:33.658Z',
-      updatedAt: '2023-11-16T17:36:33.658Z',
-    },
-  },
-  {
-    id: 18,
-    title: 'TWAMM',
-    description:
-      ' A TWAMM (Time Weighted Average Market Maker) is a type of market maker that uses time-weighted averages to calculate the prices of assets. This can be used to reduce the volatility of the market and to provide more accurate prices for assets.\n' +
-      '',
-    creator: 'Uniswap',
-    website: 'https://github.com/Uniswap/v4-periphery',
-    github:
-      'https://github.com/Uniswap/v4-periphery/blob/main/contracts/hooks/examples/TWAMM.sol',
-    status: 'published',
-    createdAt: '2023-11-16T17:33:30.819Z',
-    updatedAt: '2023-11-16T17:38:26.977Z',
-    categoryId: 'uniswap-labs',
-    category: {
-      id: 'uniswap-labs',
-      title: 'Uniswap Labs',
-      category: 'hooks',
-      emoji: '🦄',
-      count: 0,
-      tag: 'Verified',
-      createdAt: '2023-11-16T17:36:33.658Z',
-      updatedAt: '2023-11-16T17:36:33.658Z',
-    },
-  },
-  {
-    id: 20,
+    id: 125,
     title: 'Truncated Oracle',
     description:
-      'A truncated oracle is an onchain price oracle that records the price of an asset in a Uniswap liquidity pool using the geometric mean formula.\n' +
-      '',
-    creator: 'Uniswap',
+      'A truncated oracle is an onchain price oracle that records the price of an asset in a Uniswap liquidity pool using the geometric mean formula.',
+    creator: 'Uniswap Labs',
     website: 'https://github.com/Uniswap/v4-periphery',
     github:
       'https://github.com/Uniswap/v4-periphery/blob/trunc-oracle/contracts/hooks/TruncGeoOracle.sol',
     status: 'published',
-    createdAt: '2023-11-16T17:34:24.403Z',
-    updatedAt: '2023-11-16T17:38:39.448Z',
+    createdAt: '2023-11-19T08:42:58.337Z',
+    updatedAt: '2023-11-19T08:42:58.337Z',
     categoryId: 'uniswap-labs',
     category: {
       id: 'uniswap-labs',
       title: 'Uniswap Labs',
+      description: 'Hooks published by the Uniswap Foundation',
       category: 'hooks',
       emoji: '🦄',
       count: 0,
-      tag: 'Verified',
-      createdAt: '2023-11-16T17:36:33.658Z',
-      updatedAt: '2023-11-16T17:36:33.658Z',
+      tag: 'NULL',
+      createdAt: '2023-11-19T08:28:38.649Z',
+      updatedAt: '2023-11-19T08:28:38.649Z',
+    },
+  },
+  {
+    id: 126,
+    title: 'Limit Order',
+    description:
+      'A hook that allows users to place limit orders. This means that they can specify the price at which they are willing to buy or sell an asset. If the market price reaches the limit price, the order will be executed.',
+    creator: 'Uniswap Labs',
+    website: 'https://github.com/Uniswap/v4-periphery',
+    github:
+      'https://github.com/Uniswap/v4-periphery/blob/main/contracts/hooks/examples/LimitOrder.sol',
+    status: 'published',
+    createdAt: '2023-11-19T08:42:58.337Z',
+    updatedAt: '2023-11-19T08:42:58.337Z',
+    categoryId: 'uniswap-labs',
+    category: {
+      id: 'uniswap-labs',
+      title: 'Uniswap Labs',
+      description: 'Hooks published by the Uniswap Foundation',
+      category: 'hooks',
+      emoji: '🦄',
+      count: 0,
+      tag: 'NULL',
+      createdAt: '2023-11-19T08:28:38.649Z',
+      updatedAt: '2023-11-19T08:28:38.649Z',
+    },
+  },
+  {
+    id: 127,
+    title: 'TWAMM',
+    description:
+      'A TWAMM (Time Weighted Average Market Maker) is a type of market maker that uses time-weighted averages to calculate the prices of assets. This can be used to reduce the volatility of the market and to provide more accurate prices for assets.',
+    creator: 'Uniswap Labs',
+    website: 'https://github.com/Uniswap/v4-periphery',
+    github:
+      'https://github.com/Uniswap/v4-periphery/blob/main/contracts/hooks/examples/TWAMM.sol',
+    status: 'published',
+    createdAt: '2023-11-19T08:42:58.337Z',
+    updatedAt: '2023-11-19T08:42:58.337Z',
+    categoryId: 'uniswap-labs',
+    category: {
+      id: 'uniswap-labs',
+      title: 'Uniswap Labs',
+      description: 'Hooks published by the Uniswap Foundation',
+      category: 'hooks',
+      emoji: '🦄',
+      count: 0,
+      tag: 'NULL',
+      createdAt: '2023-11-19T08:28:38.649Z',
+      updatedAt: '2023-11-19T08:28:38.649Z',
+    },
+  },
+  {
+    id: 128,
+    title: 'Full Range',
+    description:
+      'A hook that allows a Uniswap pool to provide liquidity for a range of prices. This can be used to create a market maker for a volatile asset or to provide more liquidity for a thin market.',
+    creator: 'Uniswap Labs',
+    website: 'https://github.com/Uniswap/v4-periphery',
+    github:
+      'https://github.com/Uniswap/v4-periphery/blob/main/contracts/hooks/examples/FullRange.sol',
+    status: 'published',
+    createdAt: '2023-11-19T08:42:58.337Z',
+    updatedAt: '2023-11-19T08:42:58.337Z',
+    categoryId: 'uniswap-labs',
+    category: {
+      id: 'uniswap-labs',
+      title: 'Uniswap Labs',
+      description: 'Hooks published by the Uniswap Foundation',
+      category: 'hooks',
+      emoji: '🦄',
+      count: 0,
+      tag: 'NULL',
+      createdAt: '2023-11-19T08:28:38.649Z',
+      updatedAt: '2023-11-19T08:28:38.649Z',
+    },
+  },
+  {
+    id: 129,
+    title: 'Geomean Oracle',
+    description:
+      'A unique hook making a Uniswap pool function as an oracle. The geomean price is calculated using the prices of the assets in the pool. This can be used to get a more accurate price for an asset than a single oracle.',
+    creator: 'Uniswap Labs',
+    website: 'https://github.com/Uniswap/v4-periphery',
+    github:
+      'https://github.com/Uniswap/v4-periphery/blob/main/contracts/hooks/examples/GeomeanOracle.sol',
+    status: 'published',
+    createdAt: '2023-11-19T08:42:58.337Z',
+    updatedAt: '2023-11-19T08:42:58.337Z',
+    categoryId: 'uniswap-labs',
+    category: {
+      id: 'uniswap-labs',
+      title: 'Uniswap Labs',
+      description: 'Hooks published by the Uniswap Foundation',
+      category: 'hooks',
+      emoji: '🦄',
+      count: 0,
+      tag: 'NULL',
+      createdAt: '2023-11-19T08:28:38.649Z',
+      updatedAt: '2023-11-19T08:28:38.649Z',
+    },
+  },
+  {
+    id: 130,
+    title: 'Volatility Oracle',
+    description:
+      'A volatility oracle is a contract that provides information about the volatility of an asset. This information can be used to price options and other derivatives.',
+    creator: 'Uniswap Labs',
+    website: 'https://github.com/Uniswap/v4-periphery',
+    github:
+      'https://github.com/Uniswap/v4-periphery/blob/main/contracts/hooks/examples/VolatilityOracle.sol',
+    status: 'published',
+    createdAt: '2023-11-19T08:42:58.337Z',
+    updatedAt: '2023-11-19T08:42:58.337Z',
+    categoryId: 'uniswap-labs',
+    category: {
+      id: 'uniswap-labs',
+      title: 'Uniswap Labs',
+      description: 'Hooks published by the Uniswap Foundation',
+      category: 'hooks',
+      emoji: '🦄',
+      count: 0,
+      tag: 'NULL',
+      createdAt: '2023-11-19T08:28:38.649Z',
+      updatedAt: '2023-11-19T08:28:38.649Z',
     },
   },
 ]
 
 export default function PopularHooks() {
+  const hooks = uniswaplabs.sort(() => Math.random() - 0.5)
+
   return (
     <>
-      {uniswaplabs.slice(0, 5).map((item) => (
+      {hooks.slice(0, 5).map((item) => (
         <Link href={item.github} key={item.id} target="_blank">
           <Badge variant="default" className="mb-1 mr-1">
             {item.category.emoji} {item.title}
@@ -143,7 +170,7 @@ export default function PopularHooks() {
       <Badge>
         <Link
           href="/components/hooks/uniswap-labs"
-          className="flex items-center gap-2 mr-1"
+          className="mr-1 flex items-center gap-2"
         >
           <BsThreeDots />
           View more
